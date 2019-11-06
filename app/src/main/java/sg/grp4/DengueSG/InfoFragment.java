36d7febbb533193bc82d034bc2a6cd9d78bc28b3
Ming Han Brannon Chua <s10187075@connect.np.edu.sg>
2019-11-06 14:34:55 +0800

    info fragment part 1

--- app/src/main/java/sg/grp4/DengueSG/InfoFragment.java
+++ app/src/main/java/sg/grp4/DengueSG/InfoFragment.java
@@ -7,11 +7,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class InfoFragment extends Fragment {
-    @Nullable
+
+    public InfoFragment(){
+
+    }
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info, container,false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_info, container,false);
+
+        String[] dengueItems= {"Dengue sucks",
+                "test 2",
+                "test 3"};
+
+        ListView listView = (ListView) view.findViewById(R.id.lv1);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                dengueItems
+        );
+
+        listView.setAdapter(listViewAdapter);
+        return view;
     }
 }
--- app/src/main/java/sg/grp4/DengueSG/PostsFragment.java
+++ app/src/main/java/sg/grp4/DengueSG/PostsFragment.java
@@ -7,11 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PostsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_posts, container,false);
     }
 }
