63faf1c9a7ad27756a2084c207d27ceddb1721fb
Ming Han Brannon Chua <s10187075@connect.np.edu.sg>
2019-11-12 12:56:23 +0800

    info fragment finale : finished onclicklistener and added all the class files and xml files/layout

---
+++ app/src/main/java/sg/grp4/DengueSG/DetailsFragment.java
@@ -0,0 +1,19 @@
+package sg.grp4.DengueSG;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailsFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_details, container, false);
+        return view;
+    }
+}
--- app/src/main/java/sg/grp4/DengueSG/InfoFragment.java
+++ app/src/main/java/sg/grp4/DengueSG/InfoFragment.java
@@ -1,14 +1,18 @@
 package sg.grp4.DengueSG;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class InfoFragment extends Fragment {
 
@@ -21,12 +25,13 @@ public class InfoFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_info, container,false);
 
-        String[] dengueItems= {"Dengue sucks",
+        //Items in list
+        final String dengueItems[] = new String[] {"Dengue sucks",
                 "test 2",
                 "test 3"};
 
-        ListView listView = (ListView) view.findViewById(R.id.lv1);
-
+        final ListView listView = view.findViewById(R.id.lv1);
+        //ArrayAdapter for ListView lv1
         ArrayAdapter<String> listViewAdapter = new ArrayAdapter<>(
                 getActivity(),
                 android.R.layout.simple_list_item_1,
@@ -34,6 +39,42 @@ public class InfoFragment extends Fragment {
         );
 
         listView.setAdapter(listViewAdapter);
+
+        //Onclicklistener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fragment selectedFragment = null;
+
+                if ( position==0)
+                {
+                    selectedFragment = new DetailsFragment();
+                }
+                else if(position==1) {
+                    selectedFragment = new Details2Fragment();
+                }
+                else if(position==2) {
+                    selectedFragment = new Details3Fragment();
+                }
+                else if(position==3) {
+                    selectedFragment = new Details4Fragment();
+                }
+                else if(position==4) {
+                    selectedFragment = new Details5Fragment();
+                }
+                else if(position==5){
+                    selectedFragment = new Details6Fragment();
+                }
+                else if(position==6){
+                    selectedFragment = new Details7Fragment();
+                }
+                // must be after if statements
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_layout, selectedFragment );
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
         return view;
     }
 }
