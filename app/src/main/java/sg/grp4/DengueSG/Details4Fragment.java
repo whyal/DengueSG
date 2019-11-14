75d251da90373971f3eba3bb7d64590ac3048ee0
Ming Han Brannon Chua <s10187075@connect.np.edu.sg>
2019-11-14 09:26:11 +0800

    finalised infofragment detailfragment part3

--- app/src/main/java/sg/grp4/DengueSG/Details4Fragment.java
+++ app/src/main/java/sg/grp4/DengueSG/Details4Fragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Details4Fragment extends Fragment {
@@ -17,8 +19,20 @@ public class Details4Fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_details, container, false);
-        textView = view.findViewById(R.id.textView3 );
-        textView.setText("");
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_layout, new InfoFragment() );
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        //TextView
+        textView = view.findViewById(R.id.textView );
+        textView.setText("hi");
         return view;
     }
 }
