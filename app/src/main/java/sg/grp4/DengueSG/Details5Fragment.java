10a1c04bb4fa22b305c385691c1e6ea2cae8fbe8
Rasyid Waterkamp <rasyidwaterkamp@gmail.com>
2020-01-28 01:27:59 +0800

    Added stuff

--- app/src/main/java/sg/grp4/DengueSG/Details5Fragment.java
+++ app/src/main/java/sg/grp4/DengueSG/Details5Fragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,32 +31,43 @@ public class Details5Fragment extends Fragment {
                 transaction.commit();
             }
         });
+
+        String titleString = "<h1>DF vs DHF</h1>";
+        TextView title = view.findViewById(R.id.textViewTitle);
+        title.setText(Html.fromHtml(titleString));
         //TextView
         textView = view.findViewById(R.id.textView );
-        textView.setText("5.     DF vs DHF\n" +
-                "Dengue Fever rarely causes death. However, the infection can progress into a more serious condition known as the Dengue Haemorrhagic Fever. Some patients with DF go on to develop DHF. The outlook for DHF depends on how early the condition is detected. People who receive care in the early stages of dengue infection will often recover.\n" +
-                "The symptoms of DHF include:\n" +
-                "-   \tRestlessness\n" +
-                "-   \tAcute, or sudden, fever\n" +
-                "-   \tSevere abdominal pain\n" +
-                "-   \tBleeding or bruising under the skin\n" +
-                "-   \tCold or clamming skin\n" +
-                "-   \tNosebleeds\n" +
-                "-   \tLarge decrease in blood pressure.\n" +
-                "Complications from DHF may include:\n" +
-                "-   \tSeizures\n" +
-                "-   \tBrain damage\n" +
-                "-   \tBlood clots\n" +
-                "-   \tDamage to liver and lungs\n" +
-                "-   \tHeart damage\n" +
-                "-   \tShock\n" +
-                "-   \tDeath\n" +
-                "Who is at risk for DHF?\n" +
-                "-   \tInfants and small children\n" +
-                "-   \tPregnant women\n" +
-                "-   \tOlder adults\n" +
-                "-   \tThose with compromised immune system\n" +
-                "How to spot an Aedes aegypti \n");
+        String sourceString =
+                "Dengue Fever rarely causes death. However, the infection can progress into a more serious condition known as the Dengue Haemorrhagic Fever. Some patients with DF go on to develop DHF. The outlook for DHF depends on how early the condition is detected. People who receive care in the early stages of dengue infection will often recover."
+                        + "<b>" + "<h3>The symptoms of DHF include :</h3>" + "</b>"
+                        + "<ul>" +
+                            "<li>Restlessness<li>" +
+                            "<li>Acute, or sudden, fever<li>" +
+                            "<li>Severe abdominal pain<li>" +
+                            "<li>Bleeding or bruising under the skin<li>" +
+                            "<li>Cold or clamming skin<li>" +
+                            "<li>Nosebleeds<li>"
+                        + "</ul>"
+                        + "<b>" + "<h3>Complications from DHF may include :</h3>" + "</b>"
+                            + "<ul>" +
+                            "<li>Seizures<li>" +
+                            "<li>Brain damage<li>" +
+                            "<li>Blood clots<li>" +
+                            "<li>Damage to liver and lungs<li>" +
+                            "<li>Heart damage<li>" +
+                            "<li>Shock<li>" +
+                            "<li>Death<li>"
+                        + "</ul>"
+                        + "<b>" + "<h3>Who is at risk for DHF? : </h3>" + "</b>"
+                        + "<ul>" +
+                            "<li>Infants and small children<li>" +
+                            "<li>Pregnant women<li>" +
+                            "<li>Older adults<li>" +
+                            "<li>Those with compromised immune system<li>"
+                        + "</ul>"
+                ;
+
+        textView.setText(Html.fromHtml(sourceString));
         return view;
     }
 
