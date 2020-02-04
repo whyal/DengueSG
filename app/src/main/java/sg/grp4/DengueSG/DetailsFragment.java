1f7fcd21111fc2cef6ffee16527e2c4e3a6196ab
Ming Han Brannon Chua <s10187075@connect.np.edu.sg>
2020-02-04 10:26:04 +0800

    text to speech play & pause. Pause not working yet

--- app/src/main/java/sg/grp4/DengueSG/DetailsFragment.java
+++ app/src/main/java/sg/grp4/DengueSG/DetailsFragment.java
@@ -1,5 +1,6 @@
 package sg.grp4.DengueSG;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,16 +13,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class DetailsFragment extends Fragment {
     View view;
     TextView  textView;
+    MediaPlayer player;
+    Button play;
+    Button pause;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_details, container, false);
         Button button = view.findViewById(R.id.button);
+
+        //Play OnClick
+        play = view.findViewById(R.id.button2);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play(view);
+                Toast.makeText(getActivity(), "Play", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Pause OnClick
+        pause = view.findViewById(R.id.button3);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause(view);
+                Toast.makeText(getActivity(),"Paused",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +58,7 @@ public class DetailsFragment extends Fragment {
                 transaction.replace(R.id.fragment_layout, new InfoFragment() );
                 transaction.addToBackStack(null);
                 transaction.commit();
+
             }
         });
         //Title TextView
@@ -57,5 +86,37 @@ public class DetailsFragment extends Fragment {
 
         textView.setText(Html.fromHtml(sourceString));
         return view;
+    }
+
+    public void play(View v) {
+        if(player == null) {
+            player = MediaPlayer.create(getActivity(),R.raw.testfile);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+        player.start();
+    }
+
+    public void pause(View v) {
+        if (player == null) {
+            player.pause();
+        }
+    }
+
+
+    public void stop(View v){
+        stopPlayer();
+    }
+
+    private void stopPlayer() {
+        if (player != null){
+            player.release();
+            player = null;
+            Toast.makeText(getActivity(), "MediaPlayer Released", Toast.LENGTH_SHORT).show();
+        }
     }
 }
