aae995e9baf733402ae861f9f21ce7fc56ba2eb6
Rasyid Waterkamp <rasyidwaterkamp@gmail.com>
2020-02-05 15:40:50 +0800

    - Created several activities - Added Pictures - Added shopping items - Added Layouts - Working Shopping tab

---
+++ app/src/main/java/sg/grp4/DengueSG/CheckOutActivity.java
@@ -0,0 +1,13 @@
+package sg.grp4.DengueSG;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out);
+    }
+}
